e9173fbd73aee1531f95b16919dd51849740b300
sansheng <807156237@qq.com>
2020-07-23 20:05:30 +0800

    guessNumber [colin] feature : pass should_return_0A0B_when_guess_given_answer_1234_input_5678

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,11 +1,16 @@
 package example;
 
+import java.util.Arrays;
+
 /**
  * @author ck
  * @create 2020-07-23 19:57
  */
 public class GuessNumber {
     public String guess(int[] input, int[] answer) {
-        return "4A0B";
+        if(Arrays.equals(input,answer)){
+            return "4A0B";
+        }
+        return "0A0B";
     }
 }
