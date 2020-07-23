17fc472a74bb144e5757edc9fad999f99edafdb8
sansheng <807156237@qq.com>
2020-07-23 23:33:39 +0800

    guessNumber [colin] bug-fix : pass should_return_false_when_check_given_number_1125

---
+++ src/test/java/example/GameFlowTest.java
@@ -0,0 +1,20 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ck
+ * @create 2020-07-23 21:59
+ */
+public class GameFlowTest {
+
+    @Test
+    void should_return_true_when_check_given_four_number_from_console() {
+        //given
+        GameFlow gameFlow = new GameFlow();
+        //when
+
+        //then
+
+    }
+}
