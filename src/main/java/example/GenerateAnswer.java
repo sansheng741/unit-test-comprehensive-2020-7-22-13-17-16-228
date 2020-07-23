4108dc4bc4c737f5bbbd836b1c6ed2560a24a926
sansheng <807156237@qq.com>
2020-07-23 21:08:54 +0800

    guessNumber [colin] refactor : GenerateAnswer.generate

--- src/main/java/example/GenerateAnswer.java
+++ src/main/java/example/GenerateAnswer.java
@@ -1,12 +1,18 @@
 package example;
 
+import java.util.Random;
+
 /**
  * @author ck
  * @create 2020-07-23 20:49
  */
 public class GenerateAnswer {
     public int[] generate() {
-        int[] answer = {1,2,3,4};
+        Random random = new Random();
+        int[] answer = new int[4];
+        for(int i = 0; i < 4; i++){
+            answer[i] = random.nextInt(10);
+        }
         return answer;
     }
 }
--- src/test/java/example/GenerateAnswerTest.java
+++ src/test/java/example/GenerateAnswerTest.java
@@ -58,7 +58,6 @@ public class GenerateAnswerTest {
             isLegal = false;
         }
         //then
-
         Assertions.assertEquals(true, isLegal);
     }
 }
