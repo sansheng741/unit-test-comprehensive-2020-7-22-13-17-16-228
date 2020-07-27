2fae147f1545a8374eab4bc47a41cdf01011cb78
sansheng <807156237@qq.com>
2020-07-27 19:57:47 +0800

    guessNumber [colin] refactor : extract isInputLengthCorrect and isOutOfRange method

--- src/main/java/example/InputValidityCheck.java
+++ src/main/java/example/InputValidityCheck.java
@@ -7,24 +7,38 @@ package example;
 public class InputValidityCheck {
 
     public static boolean check(int[] input) {
-        if(!isNumberDifferent(input)){
+        if (!isInputLengthCorrect(input)) {
             return false;
         }
-        for(int i = 0; i < input.length; i++){
-            if(input[i] < 0 || input[i] > 9){
-                return false;
-            }
+        if (!isNumberDifferent(input)) {
+            return false;
         }
-        if(input.length != 4){
+        if (isOutOfRange(input)) {
             return false;
         }
         return true;
     }
 
-    private static boolean isNumberDifferent(int[] input){
+    private static boolean isInputLengthCorrect(int[] input) {
+        if (input.length == 4) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isOutOfRange(int[] input) {
         for (int i = 0; i < input.length; i++) {
-            for(int j = i + 1; j < input.length; j++){
-                if(input[i] == input[j]){
+            if (input[i] < 0 || input[i] > 9) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberDifferent(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] == input[j]) {
                     return false;
                 }
             }
